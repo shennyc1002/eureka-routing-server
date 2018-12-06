6c8d90ae642f5ab1c5dc5364ad6c5364647bf2bf
shennychacko <shennychacko80@gmail.com>
2018-12-05 20:19:16 -0700

    initial checkin

---
+++ src/main/java/com/example/microservices/eurekaroutingserver/EurekaRoutingServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.microservices.eurekaroutingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRoutingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRoutingServerApplication.class, args);
+	}
+}
